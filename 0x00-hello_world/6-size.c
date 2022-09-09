8e44d042420e39f423356d111ea0f61601768aad
kimgcash <olugbamidayodavid88@gmail.com>
2022-09-09 06:54:01 -0700

    Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,5 @@
-#include <stdio.h>
+#include  <stdio.h>
+
 /**
  * main - Entry point
  *
@@ -7,27 +8,15 @@
 int main(void)
 {
 	int a;
-
-		long int b;
-
-			long long int c;
-
-				char d;
-
-					float f;
-
-
-
-						printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
-
-							printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
-
-								printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
-
-									printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-
-										printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-
-											return (0);
-
+	long int b;
+	long long int c;
+	char d;
+	float f;
+	
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	return (0);
 }
