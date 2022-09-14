dc0fc3bb9cd8946853c0134edbc7fe74da4e2e97
kimgcash <olugbamidayodavid88@gmail.com>
2022-09-14 12:29:45 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - check Description
+ *
+ * Description: write a program that prints putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char nm = "kimgcash";
+
+	while (*nm)
+	{
+		_putchar(*nm);
+		nm++;
+		_putchar('\n');
+
+		return (0);
+	}
+}
