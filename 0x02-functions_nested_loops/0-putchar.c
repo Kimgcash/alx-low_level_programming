71f2598ca4a858fa1dd703b433463bf805dc0c5d
kimgcash <olugbamidayodavid88@gmail.com>
2022-09-14 14:48:07 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,21 @@
 #include "main.h"
 
 /**
-* main -print putchar
+* main - print putchar 
 * Return: always 0
 */
 
 int main(void)
 {
-	char nm = "kimgcash";
+         _putchar('_');
+         _putchar('p');
+         _putchar('u');
+         _putchar('t');
+         _putchar('c');
+         _putchar('h');
+         _putchar('a');
+         _putchar('r');
+         _putchar('\n')
 
-	while (*nm)
-	{
-		_putchar(*nm);
-		sh++;
-	}
-	_putchar('\n');
-
-	return (0);
+         return (0);
 }
