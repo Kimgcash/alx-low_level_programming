a9aac71da7b40a4357c0e0d11afc88aec34298c7
Kimgcash <olugbamidayodavid88@gmail.com>
2022-12-14 23:05:48 -0800

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,21 @@
 #include "main.h"
-
 /**
- * main - Entry point
- *
+ * main - main function .
+ * is written by Kimgcash
  * Return: 0
  */
 
 int main(void)
-
 {
-	-putchar('_');
-	-putchar('p');
-	-putchar('u');
-	-putchar('t');
-	-putchar('c');
-	-putchar('h');
-	-putchar('a');
-	-putchar('r');
-	-putchar('\n');
-
-	return (0);
+	_putchar('-');
+	_putchar('p');  
+	_putchar('u');  
+	_putchar('t');  
+	_putchar('c');  
+	_putchar('h');  
+	_putchar('a');  
+	_putchar('r');
+_putchar('\n');
+return (0);
 }
+
