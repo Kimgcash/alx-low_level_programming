7030d3e984b9fcfdaec79abc8647575f5b871d45
Kimgcash <olugbamidayodavid88@gmail.com>
2022-12-22 03:30:42 -0800

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,11 +1,10 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _strcat - concatenates two strings
  * @dest: string to append to
  * @src: string to add
- * 
+ *
  * Return: a pointer to the resulting string
  */
 char *_strcat(char *dest, char *src)
